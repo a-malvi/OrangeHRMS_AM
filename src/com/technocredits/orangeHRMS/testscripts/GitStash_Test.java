d69e6e2c20041a3c127269e5c67351ef02eaa4a3
AnkitM <ankit.malvi@gmail.com>
2021-07-09 08:14:01 +0530

    git stash learning2

--- src/com/technocredits/orangeHRMS/testscripts/GitStash_Test.java
+++ src/com/technocredits/orangeHRMS/testscripts/GitStash_Test.java
@@ -3,7 +3,7 @@ package com.technocredits.orangeHRMS.testscripts;
 public class GitStash_Test {
 	
 	public static void main(String[] args) {
-		System.out.println(1);
+		System.out.println(" The changes has been made by AM");
 		System.out.println(2);
 	}
 
